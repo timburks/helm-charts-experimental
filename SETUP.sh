4f3d791c06de42645d0db18c686a2711f556ac85
Tim Burks <timburks@google.com>
2023-04-06 22:14:01 -0700

    add registry viewer

--- SETUP.sh
+++ SETUP.sh
@@ -4,14 +4,17 @@
 # Everything is installing to a "registry" namespace.
 # YMMV.
 
-# Install the TLS certificate for the registry service host.
+# Install the TLS certificate for the registry service hosts.
 kubectl apply -f secret.yaml --namespace registry
 
 # Install postgres with the bitnami chart.
 microk8s helm3 install --namespace registry data bitnami/postgresql
 
-# Install the registry-server with a local chart.
+# Install the registry-server.
 microk8s helm3 install --namespace registry registry-server registry-server
 
-# Install registry-gateway-noauth with a local chart.
+# Install registry-gateway-noauth.
 microk8s helm3 install --namespace registry registry-gateway-noauth registry-gateway-noauth --set ingress.host=${REGISTRY_HOST}
+
+# Install registry-viewer.
+microk8s helm3 install --namespace registry registry-viewer registry-viewer --set ingress.host=${VIEWER_HOST} --set registry.host=${REGISTRY_HOST}
--- TEARDOWN.sh
+++ TEARDOWN.sh
@@ -4,10 +4,11 @@
 # Everything is installing to a "registry" namespace.
 # YMMV.
 
+microk8s helm3 uninstall --namespace registry registry-viewer
 microk8s helm3 uninstall --namespace registry registry-gateway-noauth
 microk8s helm3 uninstall --namespace registry registry-server
 microk8s helm3 uninstall --namespace registry data
 
-kubectl delete secrets/grpc-tls --namespace registry
+kubectl delete secrets/registry-tls --namespace registry
 kubectl delete pvc/data-data-postgresql-0 -n registry
 
